eda971ffa4ff58adf312dcf8188a88cea606fbb8
GustavoMeloCoelho <90584080+GustavoMeloCoelho@users.noreply.github.com>
2024-08-08 22:34:22 -0300

    finalizado o exercicio

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+function addition(event) {
+    event.preventDefault();
+    const number1 = parseInt(document.getElementById("input-number1").value); 
+    const number2 = parseInt(document.getElementById("input-number2").value); 
+    const resultado = number1+number2;
+    document.getElementById("result").innerHTML = "Resultado: "+resultado;
+}   
+
+document.getElementById("form-calculator").addEventListener("submit", addition);
+
+
