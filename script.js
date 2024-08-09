c105cbbdc16847f6b858149849b1126dd488b1e3
GustavoMeloCoelho <90584080+GustavoMeloCoelho@users.noreply.github.com>
2024-08-09 20:06:16 -0300

    adicionado opção de escolher a operação

--- script.js
+++ script.js
@@ -2,8 +2,24 @@ function addition(event) {
     event.preventDefault();
     const number1 = parseInt(document.getElementById("input-number1").value); 
     const number2 = parseInt(document.getElementById("input-number2").value); 
-    const resultado = number1+number2;
-    document.getElementById("result").innerHTML = "Resultado: "+resultado;
+    let operationResult;
+    if(document.getElementById("radio-sum").checked){
+        operationResult = number1+number2;
+    }
+
+    if(document.getElementById("radio-subtraction").checked){
+        operationResult = number1-number2;
+    }
+
+    if(document.getElementById("radio-multiply").checked){
+        operationResult = number1*number2;
+    }
+
+    if(document.getElementById("radio-divide").checked){
+        operationResult = number1/number2;
+    }
+    
+    document.getElementById("result").innerHTML = "Resultado: "+operationResult;
 }   
 
 document.getElementById("form-calculator").addEventListener("submit", addition);
